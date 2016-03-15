ca0525a8b09729a48c728f016fa66b208a14d036
shadi <shadimohagheghi@users.noreply.github.com>
2016-03-15 16:43:20 -0700

    Update multi_sim_comparison.m

--- multi_sim_comparison.m
+++ multi_sim_comparison.m
@@ -7,13 +7,13 @@
 function agent_loc = multi_sim_comparison(obstacle_configuration, density_function_type,density_function_params, num_agents,num_iterations, agent_loc)
 
 %random seed
-seed = 20;
+seed = 16;
 
 %Setup obstacles
 obstacles = get_obstacle_set(obstacle_configuration);
 
 %For now show plots of algorithm runs
-show_plot = false;
+show_plot = true;
 
 %setup density function
 global density_params ;
@@ -66,6 +66,13 @@ cost_combined = get_cost_timeline(agent_loc(3,:,:,:),obstacles,NUM_SAMPLES);
 cost_optimal  = get_cost_timeline(agent_loc(4,:,:,:),obstacles,NUM_SAMPLES);
 cost_ladybug  = get_cost_timeline(agent_loc(5,:,:,:),obstacles,NUM_SAMPLES);
 
+NUM_SAMPLES = 1000;
+kEnergy_lloyd  = get_kEnergy_timeline(agent_loc(1,:,:,:));
+kEnergy_approx  = get_kEnergy_timeline(agent_loc(2,:,:,:));
+kEnergy_combined = get_kEnergy_timeline(agent_loc(3,:,:,:));
+kEnergy_optimal  = get_kEnergy_timeline(agent_loc(4,:,:,:));
+kEnergy_ladybug  = get_kEnergy_timeline(agent_loc(5,:,:,:));
+
 
 figure(2);
 plot(v,cost_lloyd,'o',v,cost_approx,'+',v, cost_combined,'x',v,cost_optimal,'^',v,cost_ladybug,'s');
@@ -106,7 +113,12 @@ title('Coverage Control Displacement');
 xlabel('Iterations');
 ylabel('Total Displacement');
 
-
+figure(5);
+plot(v,kEnergy_lloyd,'o',v,kEnergy_approx,'+',v, kEnergy_combined,'x',v,kEnergy_optimal,'^',v,kEnergy_ladybug,'s');
+legend('Lloyd','Discrete Apx.','Local Path','Annealing','Ladybug')
+title('Coverage Control Sampled (1000) Kinetic Energy');
+xlabel('Iterations');
+ylabel('Kinetic Energy');
 end
 
 %Add an elseif clause to add new obstacles
@@ -132,6 +144,11 @@ function obstacles = get_obstacle_set(ob_config)
     if mod(ob_config,7) == 0
         obstacles(size(obstacles,1)+1,:,:) = [15,5;20,10;20,20;15,25;10,25;5,20;5,15;10,10;15,15;15,20;10,20;10,17;13,14;10,12;7,15;10,23;15,23;18,18;18,10;15,5 ];
     end
+    if mode(ob_config,11) == 0
+        %obstacles(size(obstacles,1)+1,:,:) = (0.034)*[0,0; 60,0; 60,35; 30,35; 30,45; 70,45; 70,5; 100,5; 70, 100; 70,55; 30, 55; 30, 65; 60,65; 60,95; 5,95; 5,65; 20,65; 20,55; 5,55; 5,45; 20,45; 20,35; 5,35; 0,0 ]
+        obstacles(size(obstacles,1)+1,:,:) = 0.3*[0,0; 60,0; 60,35; 30,35; 30,45; 70,45; 70,5; 95,5; 70, 95; 70,55; 30, 55; 30, 65; 60,65; 60,95; 5,95; 5,65; 20,65; 20,55; 5,55; 5,45; 20,45; 20,35; 5,35; 0,0 ]
+    end
+    
 end
 
 %Use sampling to detesrmine global cost
@@ -181,6 +198,20 @@ function cost_vec = get_cost_timeline(agent_locations,obstacles, NUM_SAMPLES)
     
     cost_vec = cost_vec ./ used_samples;
 
+end
+
+function kEnergy_vec = get_kEnergy_timeline(agent_locations)
+    %start at second position and calc distance moved
+    kEnergy_vec = zeros(size(agent_locations,2),1);
+
+    for counter =2:size(agent_locations,2)
+        %
+        kEnergy_vec(counter,1) = kEnergy_vec(counter-1,1);
+        for agent_num=1:size(agent_locations,3) %num agents
+            %Add euc distance moved
+            kEnergy_vec(counter,1) = (kEnergy_vec(counter,1) + sqrt(sum((agent_locations(1,counter,agent_num,1:2) - agent_locations(1,counter-1,agent_num,1:2)).^ 2 )))^2;
+        end
+    end
 end
 
 function movement_vec = get_displacement_vec(agent_locations)
